b1450727016bad5e2de42b5ca82bd5fe3a70332e
enzoleao <enzopinheiro6@gmail.com>
2025-09-02 22:18:33 -0300

    feat: redis host

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -9,7 +9,13 @@ import Redis from 'ioredis';
       useFactory: () => {
         const host = process.env.REDIS_HOST || 'localhost';
         const port = Number(process.env.REDIS_PORT || 6379);
-        return new Redis({ host, port });
+        return new Redis({
+          host,
+          port,
+          username: process.env.REDIS_USER || undefined,
+          password: process.env.REDIS_PASSWORD || undefined,
+          tls: process.env.REDIS_TLS === 'true' ? {} : undefined, // se seu provider exigir TLS
+        });
       },
     },
   ],
