a69c05b6b6465bef1795747764e49318506ee3e4
enzoleao <enzopinheiro6@gmail.com>
2025-09-16 13:48:53 -0300

    feat: testando

--- src/modules/reset-password/use-cases/forget-password-request/forget-password-request.use-case.ts
+++ src/modules/reset-password/use-cases/forget-password-request/forget-password-request.use-case.ts
@@ -16,25 +16,20 @@ export class ForgetPasswordRequestUseCase {
     const user = await this.findUserByEmailUseCase.execute(email);
 
     if (!user) {
-      // não vazar info se o email existe ou não
       return;
     }
 
-    // token em texto puro (vai por e-mail)
     const token = crypto.randomBytes(32).toString('hex');
 
-    // hash determinístico com sha256 (vai para o banco)
     const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
 
-    // salva no banco
     await this.createPasswordResetTokenUseCase.execute({
       user_id: user.id,
       token: hashedToken,
       expires_at: new Date(Date.now() + 1000 * 60 * 15), // 15 minutos
     });
 
-    // envia o token puro
-    this.registerNotificationService.sendResetPasswordQueue({
+    await this.registerNotificationService.sendResetPasswordQueue({
       email,
       token,
       user_name: user.name.split(' ')[0],
